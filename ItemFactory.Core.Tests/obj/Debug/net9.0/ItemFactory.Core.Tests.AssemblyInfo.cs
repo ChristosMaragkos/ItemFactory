cdc195060eeec01f000b5755f150afd383932858
Christos Maragkos <whitetowersoftware@gmail.com>
2025-08-11 22:26:34 +0300

    Update assembly informational version and sourcelink references

--- ItemFactory.Core.Tests/obj/Debug/net9.0/ItemFactory.Core.Tests.AssemblyInfo.cs
+++ ItemFactory.Core.Tests/obj/Debug/net9.0/ItemFactory.Core.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItemFactory.Core.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247c40d3bab8503f008e19d10028b45fee1a815d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfc657ba5f26b88ce64fcd825b74960c137ca7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItemFactory.Core.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItemFactory.Core.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
