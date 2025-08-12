f7f0c0005f540b8c05c0b60a570656500af36fb1
Christos Maragkos <whitetowersoftware@gmail.com>
2025-08-12 22:36:01 +0300

    Changed .NET version for tests

--- ItemFactory.Core.Tests/obj/Debug/net8.0/ItemFactory.Core.Tests.AssemblyInfo.cs
+++ ItemFactory.Core.Tests/obj/Debug/net8.0/ItemFactory.Core.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItemFactory.Core.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc195060eeec01f000b5755f150afd383932858")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItemFactory.Core.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItemFactory.Core.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
